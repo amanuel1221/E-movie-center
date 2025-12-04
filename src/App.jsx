bbcdb5fd33d359f3b48cc91a31d2180baeff7fd8
Amanuel <beginningoflife2324@gmail.com>
2025-12-04 08:09:38 -0900

    installation of react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+function App(){
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello, Tailwind CSS with Vite!
+      </h1>
+    </div>
+  );
+}
+export default App;
