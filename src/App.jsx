6942fe5389b7518bc27aae10cf9935785a67c2d3
Amanuel <beginningoflife2324@gmail.com>
2025-12-09 21:01:05 -0900

    route different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,61 @@
-import React from 'react';
-import './App.css';
-function App(){
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import BottomNav from "./components/BottomNav";
+import SearchModal from "./components/SearchModal";
+
+import HomePage from "./components/HomePage";
+import MovieDetails from "./components/MovieDetails";
+
+import PeoplePage from "./components/PeoplePage";
+import MoviesPage from "./components/MoviesPage";
+import TVPage from "./components/TVPage";
+import Footer from "./components/Footer";
+import PersonDetails from "./components/PersonDetails";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+
+  // Global dark mode (Tailwind)
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", darkMode);
+  }, [darkMode]);
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello, Tailwind CSS with Vite!
-      </h1>
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-black dark:text-white transition-colors duration-300">
+      {/* TOP NAVBAR */}
+      <Navbar
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        openSearch={() => setShowSearch(true)}
+      />
+
+      {/* SEARCH MODAL */}
+      <SearchModal isOpen={showSearch} onClose={() => setShowSearch(false)} />
+
+      {/* MAIN CONTENT */}
+      <main className="pb-28">
+        <Routes>
+          <Route path="/people" element={<PeoplePage/>} />
+          <Route path="/movies" element={<MoviesPage/>}/>
+          <Route path="/tv" element={<TVPage/>}/>
+          <Route path="/" element={<HomePage />} />
+          
+
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/tv/:id" element={<MovieDetails type="tv" />} />
+         
+          <Route path="/person/:id" element={<PersonDetails />} />
+        </Routes>
+      </main>
+
+      {/* FLOATING BOTTOM NAV */}
+      <BottomNav />
+      <Footer/>
     </div>
   );
 }
+
 export default App;
